4b703c3baf94af4d8a627b3c5e7df201bec79dbe
Harsh Garg <harsh.garg@BLRETV-C02G2730.local>
2023-05-18 15:48:31 +0530

    business logic

---
+++ JEDI-2.0-Group-A-Java-Application/src/com/flipkart/business/AdminBusiness.java
@@ -0,0 +1,17 @@
+package com.flipkart.business;
+
+public class AdminBusiness {
+	
+	public void addProff() {
+		return;
+	}
+	
+	public void assignCource() {
+		return;
+	}
+	
+	public void approveStudent() {
+		return;
+	}
+
+}
---
+++ JEDI-2.0-Group-A-Java-Application/src/com/flipkart/business/UserBusiness.java
@@ -0,0 +1,14 @@
+package com.flipkart.business;
+
+public class UserBusiness {
+	
+	public void updateDetails() {
+	
+		return;
+	}
+	
+	public void updatePasswd(){
+		return;
+	}
+	
+}
