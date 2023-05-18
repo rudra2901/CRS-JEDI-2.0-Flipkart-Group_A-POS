3bfa6e5566dcaa6ce4512391392a64a5032002ee
Rudra Tiwari <rudratiwari2901@gmail.com>
2023-05-18 16:01:42 +0530

    feat: admin client

---
+++ JEDI-2.0-Group-A-Java-Application/src/com/flipkart/client/ClientAdmin.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.client;
+
+/**
+ * @author rudra.tiwari
+ *
+ */
+public class ClientAdmin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
